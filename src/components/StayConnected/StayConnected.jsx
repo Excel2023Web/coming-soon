3ca0df851fb00faf34e95012a788834ce3b02dbb
nandiinii <preethanandini175@gmail.com>
2023-05-08 20:24:05 +0530

    Social Media Icons Added

--- src/components/StayConnected/StayConnected.jsx
+++ src/components/StayConnected/StayConnected.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
 import './StayConnected.css'
+import {FaTwitterSquare} from 'react-icons/fa'
+import {FaLinkedin} from 'react-icons/fa'
+import {FaInstagramSquare} from 'react-icons/fa'
+import {FaFacebookSquare} from 'react-icons/fa'
+
 const StayConnected = () => {
   return (
     <div className='stayconnected__container'>
-      <h1>StayConnected</h1>
+      <div>
+        <h1>Stay Connected</h1>
+      </div>
+      <div className='footer_icons'>
+        <a target='_blank' rel='noreferrer' href='https://www.instagram.com/excelmec/'>
+          <FaInstagramSquare/>
+        </a> 
+        <a target='_blank' rel='noreferrer' href='https://www.facebook.com/excelmec'>  
+          <FaFacebookSquare/>
+        </a>
+        <a target='_blank' rel='noreferrer' href='https://www.linkedin.com/company/excelmec/'>
+          <FaLinkedin/>
+        </a>
+        <a target='_blank' rel='noreferrer' href='https://twitter.com/excelmec'>
+          <FaTwitterSquare/>
+        </a>
+      </div>
     </div>
   )
 }
