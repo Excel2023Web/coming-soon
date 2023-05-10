5a82f8e22a5a0a18be333a278a6ec94bec349f01
Achyuth Mohan <achyuthmohan1@gmail.com>
2023-05-10 23:51:29 +0530

    css

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,44 +1,52 @@
-import React,{useRef, useState} from 'react'
-import './Hero.css'
-import Aftermovie from "../../assets/mp4/Aftermovie.mp4"
-import VideoDialog from './VideoDialog';
+import React, {  useState } from "react";
+import "./Hero.css";
+// import Aftermovie from "../../assets/mp4/Aftermovie.mp4";
+import VideoDialog from "./VideoDialog";
 
 const Hero = () => {
-  const btnRef = useRef(null);
-  const clipRef = useRef(null);
-  const vidRef = useRef(null);
-  const[open,setOpen]=useState(false)
-  
+  // const btnRef = useRef(null);
+  // const clipRef = useRef(null);
+  // const vidRef = useRef(null);
+  const [open, setOpen] = useState(false);
+
   const handleClick = () => {
-      // btnRef.current.classList.add('active');
-      // clipRef.current.classList.add('active');
-      setOpen(true)
-      vidRef.current.play();
+    // btnRef.current.classList.add("active");
+    // clipRef.current.classList.add("active");
+    setOpen(true);
+    // vidRef.current.play();
   };
   const handleClose = () => {
-    // btnRef.current.classList.remove('active');
-    // clipRef.current.classList.remove('active');
-    vidRef.current.pause();
-    setOpen(false)
+    // btnRef.current.classList.remove("active");
+    // clipRef.current.classList.remove("active");
+    // vidRef.current.pause();
+    setOpen(false);
   };
   return (
-    <div className='hero__section_main'>
-      <VideoDialog open={open} handleClose={handleClose} clipRef={clipRef} movie={Aftermovie} vidRef={vidRef}/>
-        <img className='excel_logo' src="https://www.excelmec.org/static/media/home_logo_excel.932cc8102da68199bc0f34683e7527c1.svg" alt="" />
-        <h1>We Are Coming Soon..</h1>
-        <p>The national techno managerial symposium of Govt. Model Engineering College</p>
-        <>
-        <div className='video_btn' ref={btnRef} onClick={handleClick}>
+    <div className="hero__section_main">
+      <VideoDialog open={open} handleClose={handleClose}/>
+      <img
+        className="excel_logo"
+        src="https://www.excelmec.org/static/media/home_logo_excel.932cc8102da68199bc0f34683e7527c1.svg"
+        alt=""
+      />
+      <h1>We Are Coming Soon..</h1>
+      <p>
+        The national techno managerial symposium of Govt. Model Engineering
+        College
+      </p>
+      <div className='video_btn'  onClick={handleClick}>
           <div className='play'></div>
         </div>
-        {/* <div className='video_clip' ref={clipRef}>
+      
+      {/* <div className='video_btn' ref={btnRef} onClick={handleClick}>
+          <div className='play'></div>
+        </div> */}
+      {/* <div className='video_clip' ref={clipRef}>
             <video src={Aftermovie} ref={vidRef} controls></video>
             <b class="close" onClick={handleClose}>Close</b>
         </div> */}
-
-        </>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
--- src/components/Hero/VideoDialog.jsx
+++ src/components/Hero/VideoDialog.jsx
@@ -1,23 +1,25 @@
 import React from "react";
 import { Dialog } from "@mui/material";
-const VideoDialog = ({ handleClose, open, vidRef, clipRef, movie }) => {
+const VideoDialog = ({ handleClose, open }) => {
   return (
     <Dialog
       fullWidth={true}
       maxWidth={"md"}
       PaperProps={{
-        sx: { width: "100%", borderRadius: 5, m: 2, minHeight: "60vh" },
+        sx: { width: "100%", borderRadius: '20px', m: 2,backgroundColor: 'transparent',overflow:'hidden'},
+        style: { backgroundColor: 'transparent' }
       }}
       open={open}
       onClose={handleClose}
       aria-labelledby="alert-dialog-title"
       aria-describedby="alert-dialog-description"
     >
-      <div className="video_clip" ref={clipRef}>
-        <video src={movie} ref={vidRef} controls></video>
-        <b class="close" onClick={handleClose}>
-          Close
-        </b>
+      <div className="video_clip">
+        <video
+          src="https://excel-22-website.s3.ap-south-1.amazonaws.com/coming-soon/Comin%20Soon_1.mp4"
+          muted
+          autoPlay
+        ></video>
       </div>
     </Dialog>
   );
