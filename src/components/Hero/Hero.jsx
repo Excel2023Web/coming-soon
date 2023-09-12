f5a55eb85c1e09f5d1b1f7a67d3412add1e8ed58
nandiinii <preethanandini175@gmail.com>
2023-09-12 20:21:49 +0530

    Logo Hyperlink

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -6,6 +6,10 @@ import excel2022 from "../../assets/png/excel2023.png";
 const Hero = () => {
   const [open, setOpen] = useState(false);
 
+  const handleClickImage = () => {
+    const externalURL = "https://www.excelmec.org/";
+    window.location.href = externalURL;
+  };
   const handleClick = () => {
     setOpen(true);
   };
@@ -16,12 +20,11 @@ const Hero = () => {
     <div className="hero__section_main">
       <VideoDialog open={open} handleClose={handleClose} />
       <div className="excel__logo_container">
-        <img className="excel_logo" src={excel2022} alt="" />
+          <img className="excel_logo" src={excel2022} alt="" onClick={handleClickImage}/>
         <p>Excel 2023</p>
       </div>
       <div className="hero__desc_area">
-        <h1>We are coming soon...</h1>
-        {/* <h3>Excel is evolving!</h3> */}
+        <h1>We Are Coming Soon...</h1>
         <p>
           The national techno-managerial symposium of Govt. Model Engineering
           College
