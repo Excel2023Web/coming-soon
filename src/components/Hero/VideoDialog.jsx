8881a4e81e894aa829fcfb50e82592679c944741
Achyuth Mohan <achyuthmohan1@gmail.com>
2023-10-08 00:18:23 +0530

    css

--- src/components/Hero/VideoDialog.jsx
+++ src/components/Hero/VideoDialog.jsx
@@ -1,49 +1,62 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Dialog } from "@mui/material";
-// import afterMovie from "../../assets/mp4/aftermovie.mp4";
 
 const VideoDialog = ({ handleClose, open }) => {
+  const videoRef = useRef(null);
+
+  const handleVideoClick = () => {
+    if (videoRef.current) {
+      if (videoRef.current.paused) {
+        videoRef.current.play();
+      } else {
+        videoRef.current.pause();
+      }
+    }
+  };
+
   return (
     <Dialog
-  fullWidth={true}
-  maxWidth={"md"}
-  PaperProps={{
-    sx: {
-      width: "100%",
-      borderRadius: "20px",
-      m: 2,
-      backgroundColor: "transparent",
-      overflow: "hidden",
-    },
-    style: { backgroundColor: "transparent" },
-  }}
-  slotProps={{
-    backdrop: {
-      style: {
-        opacity: 0.5,
-        background:
-          "linear-gradient(to right, #e96443, #904e95)",
-      },
-    },
-  }}
-  open={open}
-  onClose={handleClose}
-  aria-labelledby="alert-dialog-title"
-  aria-describedby="alert-dialog-description"
->
-  <div className="video-container">
-    <video preload="auto" autoPlay>
-      <source
-        src="https://storage.googleapis.com/excel-mec-2023-static-8267634/aftermovie2022-h264.mp4"
-        type="video/mp4"
-      ></source>
-      {/* <source
-        src="https://storage.googleapis.com/excel-mec-2023-static-8267634/aftermovie2022.webm"
-        type="video/webm"
-      ></source> */}
-    </video>
-  </div>
-</Dialog>
+      fullWidth={true}
+      maxWidth={"md"}
+      PaperProps={{
+        sx: {
+          width: "100%",
+          borderRadius: "20px",
+          m: 2,
+          backgroundColor: "transparent",
+          overflow: "hidden",
+        },
+        style: { backgroundColor: "transparent" },
+      }}
+      slotProps={{
+        backdrop: {
+          style: {
+            opacity: 0.5,
+            background:
+              "linear-gradient(to right, #e96443, #904e95)",
+          },
+        },
+      }}
+      open={open}
+      onClose={handleClose}
+      aria-labelledby="alert-dialog-title"
+      aria-describedby="alert-dialog-description"
+    >
+      <div className="video-container">
+        <video
+          ref={videoRef}
+          preload="auto"
+          autoPlay
+          onClick={handleVideoClick}
+          onTouchStart={handleVideoClick}
+        >
+          <source
+            src="https://storage.googleapis.com/excel-mec-2023-static-8267634/aftermovie2022-h264.mp4"
+            type="video/mp4"
+          ></source>
+        </video>
+      </div>
+    </Dialog>
   );
 };
 
