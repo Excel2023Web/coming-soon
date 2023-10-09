f685271cb685d72a3b2569959491f43a28eb46b5
nandiinii <preethanandini175@gmail.com>
2023-10-09 11:37:39 +0530

    hyperlink changed

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -6,10 +6,10 @@ import excel2023 from "../../assets/png/excel-logo-with-text-2023.png";
 const Hero = () => {
   const [open, setOpen] = useState(false);
 
-  const handleClickImage = () => {
-    const externalURL = "https://www.excelmec.org/";
-    window.location.href = externalURL;
-  };
+  // const handleClickImage = () => {
+  //   const externalURL = "https://www.excelmec.org/";
+  //   window.location.href = externalURL;
+  // };
   const handleClick = () => {
     setOpen(true);
   };
@@ -20,8 +20,8 @@ const Hero = () => {
     <div className="hero__section_main">
       <VideoDialog open={open} handleClose={handleClose} />
       <div className="excel__logo_container">
-          <img className="excel_logo" src={excel2023} alt="" onClick={handleClickImage}/>
-        {/* <p>Excel 2023</p> */}
+          <img className="excel_logo" src={excel2023} alt=""/>
+        {/* <p>Excel 2023</p>onClick={handleClickImage} */}
       </div>
       <div className="hero__desc_area">
         <h1>Evolution in progress...</h1>
